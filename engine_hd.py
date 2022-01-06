1ba1d6164825b4033c7fab73b731a3fe1c9014e9
Andruchelli <dron_e@bk.ru>
2022-01-06 15:24:55 +0300

    1

--- engine_hd.py
+++ engine_hd.py
@@ -1,21 +1,24 @@
+from hero import *
+from dragon import *
+
 class Engine:
     def __init__(self):
         print("Добро пожаловать на игру \"Герой против Дракона\"")
 
     def next_turn(self, turn = 1):
         if turn % 2 != 0: # при нечётном числе ход за героем
-            user = input("Выберите следующее действие: атаковать или пить зелье")
+            user = input("Выберите следующее действие: атаковать или пить зелье: ")
             if user == "атаковать":
-                self.hero.attack()
+                self.hero.attack(self.hero)
             if user == "пить зелье":
-                self.hero.drink_potion()
+                self.hero.drink_potion(self.hero)
 
         else:
-            self.dragon.attack()
+            self.dragon.attack(self.dragon)
 
         turn += 1
 
-        if self.hero.health() == 0:
+        if self.hero.health(self.hero) == 0:
             print("Дракон победил")
-        if self.dragon.health() == 0:
+        if self.dragon.health(self.dragon) == 0:
             print("Герой победил")
---
+++ runner.py
@@ -0,0 +1,6 @@
+from hero import *
+from dragon import *
+from engine_hd import *
+
+engine = Engine()
+engine.next_turn()
